51e7f4a0e3fc034560b5f1efc8a1d9e35916f8e2
mjhcc365 <2209011911@qq.com>
2025-01-21 16:45:19 +0800

    text: 增加framer-motion示例

--- app/modules/Motion/index.tsx
+++ app/modules/Motion/index.tsx
@@ -8,6 +8,7 @@
 // import ScrollLinked from "./ScrollLinked";
 // import TrackElementWithinViewport from "./TrackElementWithinViewport";
 import LayoutAnimation from "./LayoutAnimation";
+import MotionEG from "./Text";
 // 如何设计动画
 // 分别是Tween（补间动画）Spring（弹性），Inertia（惯性）
 // Tween（补间动画 加速运动、减速运动、匀速运动 duration或者ease
@@ -22,7 +23,8 @@ export default function App() {
   //   return <HTMLContent />;
   //   return <ScrollLinked />;
   //   return <TrackElementWithinViewport />;
-  return <LayoutAnimation />;
+  //   return <LayoutAnimation />;
+  return <MotionEG />;
 
   return <div>22</div>;
 }
