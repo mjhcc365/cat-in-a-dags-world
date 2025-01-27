96299c811db8b28e32a7fb34e8aca658555caefe
mjhcc365 <2209011911@qq.com>
2025-01-27 09:06:46 +0800

    feat:实现滚动条

--- app/modules/Lenis/LenisAndGsap.tsx
+++ app/modules/Lenis/LenisAndGsap.tsx
@@ -1,10 +1,8 @@
 import { ReactLenis, useLenis } from "lenis/react";
 import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useStore } from "./useStore";
-import Lenis from "lenis";
 
 import { useScroll } from "./useScroll";
 
@@ -15,8 +13,12 @@ ScrollTrigger.defaults({ markers: true });
 function App() {
   const lenis = useLenis();
 
-  useScroll(({ scroll }: any) => {
-    console.log("===>scroll", scroll);
+  useScroll(({ scroll, limit }: any) => {
+    const progress = scroll / limit;
+    console.log("===>scroll", scroll, limit, progress);
+    gsap.to(".scrollbar-inner", {
+      scaleX: progress,
+    });
   });
   const [setLenis] = useStore((state: any) => [state.setLenis]);
   useEffect(() => {
@@ -40,7 +42,26 @@ function App() {
         <div style={{ height: 600 }} className="bg-red ">
           height:600px
         </div>
-        <div className="box border w-100 h-100">box</div>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            zIndex: 100,
+            width: "100%",
+          }}
+          className="scrollbar h-4"
+        >
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+              transform: "scaleX(0)", // scaleX(0) 表示将元素在水平方向上缩放到 0,百分比，小数
+              transformOrigin: "0 50%", // 表示在元素的左边缘（0%）和垂直中心（50%）处进行缩放
+            }}
+            className="scrollbar-inner h-2 bg-pink-light"
+          ></div>
+        </div>
       </div>
     </ReactLenis>
   );
